f0d7693a042802324b86ea700f61328bd3c3da3f
José Carlos Alarcón Murillo <josecarlos.alar@gmail.com>
2023-02-05 20:38:57 +0100

    update babelrc

--- src/list.tsx
+++ src/list.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, generatePath } from "react-router-dom";
 
 interface MemberEntity {
   id: string;
@@ -18,20 +18,18 @@ export const ListPage: React.FC = () => {
 
   return (
     <>
-      <h2>Hello from List page</h2>+{" "}
       <div className="list-user-list-container">
-        <span className="list-header">Avatar</span>
-        <span className="list-header">Id</span>
-        <span className="list-header">Name</span>
-        {members.map((member) => (
-          <>
-            <img src={member.avatar_url} />
-            <span>{member.id}</span>
-            <Link to={`/detail/${member.login}`}>{member.login}</Link>
-          </>
-        ))}
-      </div>
-      <Link to="/detail">Navigate to detail page</Link>
+       <span className="list-header">Avatar</span>
+       <span className="list-header">Id</span>
+       <span className="list-header">Name</span>
+       {members.map((member) => (
+         <>
+           <img src={member.avatar_url} />
+           <span>{member.id}</span>
+           <Link to={generatePath('/detail/:id', {id: member.login})}>{member.login}</Link>
+         </>
+       ))}
+     </div>
     </>
   );
 };
